3a6cb95604dbeca2e36b72f04a9f7d2f82685700
AndrewJBateman <gomezbateman@yahoo.com>
2021-06-09 12:51:30 +0200

    fix: custom schema added

--- src/app/pages/second/second.module.ts
+++ src/app/pages/second/second.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,6 +15,9 @@ import { SecondPage } from './second.page';
     IonicModule,
     SecondPageRoutingModule
   ],
-  declarations: [SecondPage]
+  declarations: [SecondPage],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class SecondPageModule {}
